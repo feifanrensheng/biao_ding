0198469936da720dfc7ae633ce710f47c90b8207
feifanrensheng <feifanrensheng1@outlook.com>
2018-03-05 19:46:53 +0800

    add files

---
+++ build/CMakeFiles/calibration_artificial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/calibration_artificial.dir/calibration_artificial.cpp.o"
+  "calibration_artificial.pdb"
+  "calibration_artificial"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/calibration_artificial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
